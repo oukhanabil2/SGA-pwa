6d6bf0408a30de41370c8b04827d637e3135b554
oukhanabil2 <oukhanabil2@gmail.com>
2025-12-25 22:05:32 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+// data.js
+const agents = [
+    {"code": "CPA", "nom": "OUKHA", "prenom": "NABIL", "groupe": "A", "tel": "0681564713", "poste": "CP", "matricule": "S09278C"},
+    {"code": "CONA", "nom": "EL JAMALI", "prenom": "Younes", "groupe": "A", "tel": "0663290648", "poste": "CON", "matricule": "S09425C"},
+    {"code": "MOTA", "nom": "TISSIRT", "prenom": "hakim", "groupe": "A", "tel": "0611160166", "poste": "MOT", "matricule": "S09279C"},
+    {"code": "ZA", "nom": "DRAFA", "prenom": "Noureddine", "groupe": "A", "tel": "603482589", "poste": "ZA", "matricule": "S09179C"},
+    {"code": "Z2A", "nom": "KAROUCHE", "prenom": "Fayçal", "groupe": "A", "tel": "vide", "poste": "Z2A", "matricule": "S13273C"},
+    {"code": "Z5A", "nom": "LAWRIQAT TARIK", "prenom": "TARIK", "groupe": "A", "tel": "615296161", "poste": "Z5A", "matricule": "S11699C"},
+    {"code": "Z6C", "nom": "HARBIL", "prenom": "ANASS", "groupe": "C", "tel": "669001099", "poste": "z6A", "matricule": "S09153C"},
+    {"code": "Z7A", "nom": "CHERKAOUI", "prenom": "NOUAMANA", "groupe": "A", "tel": "DP1400", "poste": "Z7A", "matricule": "S11869C"},
+    {"code": "O1aA", "nom": "ALAMI", "prenom": "ZAKARIA", "groupe": "A", "tel": "660269360", "poste": "O1a", "matricule": "S09188C"},
+    {"code": "O1bA", "nom": "EL KADMIRI", "prenom": "YASSINE", "groupe": "A", "tel": "707937021", "poste": "O1bA", "matricule": "S12667C"},
+    {"code": "O3A", "nom": "EL GHALLA", "prenom": "ABDELALI", "groupe": "A", "tel": "663391782", "poste": "O3A", "matricule": "S09216C"},
+    {"code": "O4B", "nom": "ANQACH HASSAN", "prenom": "HASSAN", "groupe": "B", "tel": "vide", "poste": "O4", "matricule": "RETRAITE"},
+    {"code": "O5A", "nom": "LEKHEL", "prenom": "RACHID", "groupe": "A", "tel": "644734747", "poste": "O5", "matricule": "RETRAITE"},
+    {"code": "O8A", "nom": "AIT LMKADAM", "prenom": "KAHCEN", "groupe": "A", "tel": "626521862", "poste": "O8", "matricule": "S09229C"},
+    {"code": "O9A", "nom": "OUTANOUT", "prenom": "OMAR", "groupe": "A", "tel": "6943677602", "poste": "O9A", "matricule": "S09251C"},
+    {"code": "O10A", "nom": "ZOUHRI", "prenom": "HAMID", "groupe": "A", "tel": "625615979", "poste": "O10", "matricule": "S09861C"},
+    {"code": "L11A", "nom": "QOTBI", "prenom": "OTMAN", "groupe": "A", "tel": "681688161", "poste": "L11A", "matricule": "S09156C"},
+    {"code": "L14A", "nom": "BOUNHAR", "prenom": "MOHAMED", "groupe": "A", "tel": "614445839", "poste": "L14A", "matricule": "S09235C"},
+    {"code": "CPB", "nom": "CHMAREKH", "prenom": "Noureddine", "groupe": "B", "tel": "660337343", "poste": "CPA", "matricule": "S09274C"},
+    {"code": "MOTB", "nom": "KAALI", "prenom": "MAJID", "groupe": "B", "tel": "0777934644", "poste": "MOT", "matricule": "S12666C"},
+    {"code": "Z2B", "nom": "TSOULI", "prenom": "ADIL", "groupe": "B", "tel": "0767872200", "poste": "Z2A", "matricule": "S09170C"},
+    // ... Ajoutez les autres lignes sur ce modèle
+];
+
+console.log("Base de données chargée : " + agents.length + " agents.");
